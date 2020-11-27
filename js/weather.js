b8c9be2175b21a61ff8ac8c28da9919fc7876f96
huangka <huangka@umich.edu>
2020-11-27 10:21:34 -0500

    fixed stuff

--- js/weather.js
+++ js/weather.js
@@ -1,114 +1,117 @@
 function gettingJSON(){
     
-     //Display the forecast
-    // Your code here.
+    //Display the forecast
+   // Your code here.
 
-    //Set default location if one isn't provided
-    let location;
-    
-    location = document.querySelector("#location").value;
-    if (location == "") {
-        location = "Ann Arbor";
+   //Set default location if one isn't provided
+   let location;
+   
+   location = document.querySelector("#location").value;
+   if (location == "") {
+       location = "Ann Arbor";
 
-    }
-    // Your code here.
-    console.log("Location is : " + location);
-
-    
-
-    //set default temperature format if one isn't provided
-    let format;
-    let units;
-
-    if (document.querySelector("#celcius").checked) {
-        format = "Celcius"
-        units = document.querySelector("#celcius").getAttribute("value");
-    }
-    else {
-        format = "Fahrenheit";
-        units = document.querySelector("#fahrenheit").getAttribute("value");
-    }
-
-
-    // Your code here.
-    console.log("Format is " + format);
+   }
+   // Your code here.
+   console.log("Location is : " + location);
 
    
-    //set the query  
-    let query;
-    let city;
-    let zip;
-    let countryCode;
-    key = "76e350a98a4f0d541165972767f3314c";
-    isCity = false;
-    
-    let locationArray = location.split(",");
-    const locationArrayLength = locationArray.length;
-    console.log(locationArray);
-    //check to see if this is not a number
-    if (isNaN(locationArray[0]) == true) {
-        city = locationArray[0];
-        isCity = true;
-    } else {
-        zip = locationArray[0];
-    }
 
-    if (locationArrayLength == 2) {
-        countryCode = locationArray[1];
-        countryCode= countryCode.replace(/\s/g, '');
-    
-    }
+   //set default temperature format if one isn't provided
+   let format;
+   let units;
+
+   if (document.querySelector("#celcius").checked) {
+       format = "Celcius"
+       units = document.querySelector("#celcius").getAttribute("value");
+   }
+   else {
+       format = "Fahrenheit";
+       units = document.querySelector("#fahrenheit").getAttribute("value");
+   }
+
+
+   // Your code here.
+   console.log("Format is " + format);
+
   
-   //Default A2
-   if (isCity == true && locationArrayLength == 1){
-       query = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key + "&units=" + units;
-    } else if (isCity == true && locationArrayLength == 2){
-       query = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "," + countryCode +  "&appid=" + key + "&units=" + units;
-    } else {
-        console.log('inside zip condition, printing countryCode: ', countryCode)
-        query = "https://api.openweathermap.org/data/2.5/weather?zip=" + zip + "," + countryCode + "&appid=" + key + "&units=" + units;
-    }
+   //set the query  
+   let query;
+   let city;
+   let zip;
+   let countryCode;
+   key = "76e350a98a4f0d541165972767f3314c";
+   isCity = false;
+   
+   let locationArray = location.split(",");
+   const locationArrayLength = locationArray.length;
+   console.log(locationArray);
+   //check to see if this is not a number
+   if (isNaN(locationArray[0]) == true) {
+       city = locationArray[0];
+       isCity = true;
+   } else {
+       zip = locationArray[0];
+       if (locationArrayLength == 1) {
+           countryCode = "US";
+       }
+   }
+
+   if (locationArrayLength == 2) {
+       countryCode = locationArray[1];
+       countryCode= countryCode.replace(/\s/g, '');
+   
+   }
+ 
+  //Default A2
+  if (isCity == true && locationArrayLength == 1){
+      query = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key + "&units=" + units;
+   } else if (isCity == true && locationArrayLength == 2){
+      query = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "," + countryCode +  "&appid=" + key + "&units=" + units;
+   } else {
+   
+       query = "https://api.openweathermap.org/data/2.5/weather?zip=" + zip + "," + countryCode + "&appid=" + key + "&units=" + units;
+   }
+  
+  
+   // Your code here.  
+   
+   console.log("Query is :" + query);
+
+   //Create and set variables for each of the elements you
+   //need to update, location, highs and lows, 
+   //the image, etc.
+
+   let loc = document.querySelector("#loc");
+   let temp = document.querySelector("#temp");
+   let tempImg = document.querySelector("#tempImg");
+   // Your code here.
+
    
    
-    // Your code here.  
-    
-    console.log("Query is :" + query);
+   $.getJSON(query,function(json){
+       //Use returned json to update the values of the three 
+       //elements in HTML.  
+       //I would print the JSON to the console
+       // Your code here.
+       console.log(json);
+       loc.innerHTML = json["name"];
+       temp.innerHTML = json["main"]["temp"] + " with " + json["weather"][0]["description"];
+       tempImg.setAttribute("src", "http://openweathermap.org/img/wn/" + json["weather"][0]["icon"] + ".png");
+       tempImg.setAttribute("alt", json["weather"][0]["description"]);
+       
+   
 
-    //Create and set variables for each of the elements you
-    //need to update, location, highs and lows, 
-    //the image, etc.
-
-    let loc = document.querySelector("#loc");
-    let temp = document.querySelector("#temp");
-    let tempImg = document.querySelector("#tempImg");
-    // Your code here.
-
-    
-    
-    $.getJSON(query,function(json){
-        //Use returned json to update the values of the three 
-        //elements in HTML.  
-        //I would print the JSON to the console
-        // Your code here.
-        console.log(json);
-        loc.innerHTML = json["name"];
-        temp.innerHTML = json["main"]["temp"] + " with " + json["weather"][0]["description"];
-        tempImg.setAttribute("src", "http://openweathermap.org/img/wn/" + json["weather"][0]["icon"] + ".png");
-        tempImg.setAttribute("alt", json["weather"][0]["description"]);
-        
-    
-
-    });
-    document.querySelector("#forecast").style.display = "block";
+   });
+   document.querySelector("#forecast").style.display = "block";
 };
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
